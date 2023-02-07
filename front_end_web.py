[Fixed] Error rotation & update to gradio 3.17.0

front_end_web.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import json
 import os
 import gradio as gr
 import numpy as np
@@ -6,7 +7,8 @@ import cv2
 import io
 from PIL import Image
 import matplotlib.pyplot as plt
-import json
+plt.switch_backend('Agg')
+
 
 # colors for visualization
 COLORS = [
@@ -25,6 +27,10 @@ ARTICLE = '<center><img src="https://clientscan.co.uk" alt=""/></center>'
 # Function to run the app
 # contain npy for embedings and registration photos
 DATA_DIRECTORY = 'data'
+REGISTRATION_DIRECTORY = 'registraion'
+
+if os.path.exists(os.path.join(DATA_DIRECTORY, REGISTRATION_DIRECTORY)):
+    os.mkdir(REGISTRATION_DIRECTORY)
 
 # Init models face detection & recognition
 weights = os.path.join(DATA_DIRECTORY, "models",
@@ -33,43 +39,75 @@ face_detector = cv2.FaceDetectorYN_create(weights, "", (0, 0))
 weights = os.path.join(DATA_DIRECTORY, "models", "face_recognizer_fast.onnx")
 face_recognizer = cv2.FaceRecognizerSF_create(weights, "")
 
+
 def set_image(image):
     return gr.Image.update(value=image[0][0])
+
 
 def fig2img(fig):
     buf = io.BytesIO()
     fig.savefig(buf)
     buf.seek(0)
     pil_img = Image.open(buf)
-    basewidth = 750
+    basewidth = 1000
     wpercent = (basewidth/float(pil_img.size[0]))
     hsize = int((float(pil_img.size[1])*float(wpercent)))
     img = pil_img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
     return img
 
+
 def crop_face_aligned_face(aligned_face, name_file):
     # print(file_name)
-    cv2.imwrite(os.path.join(DATA_DIRECTORY, 'registration', name_file + '.png'), aligned_face)
+    cv2.imwrite(os.path.join(DATA_DIRECTORY, 'registration',
+                name_file + '.png'), aligned_face)
+
 
 def visualize_prediction(img, faces):
     plt.figure(figsize=(50, 50))
     plt.imshow(img)
     ax = plt.gca()
     colors = COLORS * 100
-    for  face, color in zip(faces, colors):
-        (x, y, h, w) = face[:4] #list(map(int, face[:4]))
+    for face, color in zip(faces, colors):
+        (x, y, h, w) = face[:4]  # list(map(int, face[:4]))
         score = face[-1]
-        ax.add_patch(plt.Rectangle((x, y), h, w, fill=False, color=color, linewidth=10))
+        ax.add_patch(plt.Rectangle(
+            (x, y), h, w, fill=False, color=color, linewidth=10))
         ax.text(x, y, f"Confidence: {int(score * 100)}% ", fontsize=55, bbox=dict(
             facecolor="yellow", alpha=0.5))
     plt.axis("off")
     return fig2img(plt.gcf())
 
+
+def visualize_cv2(image, faces):
+    for idx, face in enumerate(faces):
+        box = list(map(int, face[:4]))
+        color = (0, 255, 0) 
+        thickness = 2
+        cv2.rectangle(image, box, color, thickness, cv2.LINE_AA)
+
+        text = f"Confidence : {face[-1]}"
+        position = (box[0], box[1] - 10)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        scale = 0.6
+        cv2.putText(image, text, position, font, scale,
+                    color, thickness, cv2.LINE_AA)
+    return image
+
+def get_biggest_face(faces):
+    max_index = 0
+    max_size = 0.0
+    for idx, face in enumerate(faces):
+        if face[2] * face[3] > max_size:
+            max_size = face[2] * face[3]
+            max_index = idx
+    return max_index
+
+
 def detect(image,
            face_score_threshold,
-           name_box, 
+           name_box,
            id_box):
-    
+
     # Gradio.Image read in numpy Image, which in channels = BRG
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -83,50 +121,50 @@ def detect(image,
     if channels == 4:
         image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
-    if image.shape[0] > 1000:
-        image = cv2.resize(image, (0, 0),
-                           fx=(500 / image.shape[0]), fy=(500 / image.shape[0]))
-    
+    image = cv2.resize(image, (0, 0),
+                       fx=(500 / image.shape[0]), fy=(500 / image.shape[0]))
+
     height, width, _ = image.shape
     face_detector.setInputSize((width, height))
-    
+
     # FACE DETECTION
     _, faces = face_detector.detect(image)
-    faces = faces if faces is not None else []
-    
-    # Get feature
-    if len(faces) == 1:
-        aligned_face = face_recognizer.alignCrop(image, faces[0])
-                         
-        # Write cropped image
-        crop_face_aligned_face(aligned_face, f'{name_box}_{id_box}')
 
-        # FACE RECOGNITION
-        feat = face_recognizer.feature(aligned_face)
-        
-        # Data to be written
-        file_feat = os.path.join(DATA_DIRECTORY, 'registration',f'{name_box}_{id_box}.npy')
-        np.save(file_feat, feat) 
-        
-        # Write metadata
-        dictionary = {
-            "name": name_box,
-            "id_number": id_box,
-            "feat_file": file_feat
-        }
-    
-        # Serializing json
-        json_object = json.dumps(dictionary, indent=4)
-        
-        # Writing to sample.json
-        with open(os.path.join(DATA_DIRECTORY, 'registration',f'{name_box}_{id_box}.json'), "w") as outfile:
-            outfile.write(json_object)
-    
+    # Get feature
+    max_idx = get_biggest_face(faces)
+    aligned_face = face_recognizer.alignCrop(image, faces[max_idx])
+
+    # Write cropped image
+    crop_face_aligned_face(aligned_face, f'{name_box}_{id_box}')
+
+    # FACE RECOGNITION
+    feat = face_recognizer.feature(aligned_face)
+
+    # Data to be written
+    file_feat = os.path.join(
+        DATA_DIRECTORY, 'registration', f'{name_box}_{id_box}.npy')
+    np.save(file_feat, feat)
+
+    #  Write metadata
+    dictionary = {
+        "name": name_box,
+        "id_number": id_box,
+        "feat_file": file_feat
+    }
+
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+
+    # Writing to sample.json
+    with open(os.path.join(DATA_DIRECTORY, 'registration', f'{name_box}_{id_box}.json'), "w") as outfile:
+        outfile.write(json_object)
+
     # assert len(faces) == 1, "THE PHOTO SHOULD CONTAIN ONLY 1 FACE"
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     showed_image = visualize_prediction(image, faces)
-
+    # showed_image = visualize_cv2(image, faces)
     return showed_image
+
 
 def interface() -> None:
     """
@@ -181,10 +219,10 @@ def interface() -> None:
                                 paths = [["data/examples/" + example]
                                          for example in os.listdir("data/examples")
                                          if '.jpg' in example]
-                                example_images = gr.Dataset(
-                                    components=([image_in]),
-                                    label="Example images",
-                                    samples=[[path] for path in paths])
+                                # example_images = gr.Dataset(
+                                #     components=([image_in]),
+                                #     label="Example images",
+                                #     samples=[[path] for path in paths])
                         with gr.Column():
                             with gr.Row():
                                 face_detected_image_out = gr.Image(
@@ -194,8 +232,8 @@ def interface() -> None:
                                     article=ARTICLE,
                                     allow_flagging="never")
 
-                            example_images.click(fn=set_image, inputs=[
-                                                 example_images], outputs=[image_in])
+                            # example_images.click(fn=set_image, inputs=[
+                            #                      example_images], outputs=[image_in])
 
                             detect_button.click(fn=detect,
                                                 inputs=[
@@ -235,6 +273,7 @@ def interface() -> None:
             show_error=True)
 
     gr.close_all()
+
 
 if __name__ == '__main__':
     interface()  # Run the interface